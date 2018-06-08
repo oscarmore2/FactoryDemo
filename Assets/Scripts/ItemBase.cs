15c23b7f264832e6cc67f03e2ac28eccb372b655
池 <754936408@qq.com>
2018-06-08 10:24:53 +0800

    first day

---
+++ Assets/Scripts/ItemBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+public enum EItemType
+{
+    EItemType_Tool,
+    EItemType_Material,
+    EItemType_Machine,
+}
+
+[System.Serializable]
+public class ItemInfo
+{
+    public Sprite imagePath;
+    public string name;
+    public EItemType itemType;
+}
+
+public interface IItem
+{
+    void OnGazeStart();
+    void OnGazeEnd();
+    void OnPicked();
+    ItemInfo OnGetItemInfo();
+}
